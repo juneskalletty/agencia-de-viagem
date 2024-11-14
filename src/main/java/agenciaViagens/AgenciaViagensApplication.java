5bf8eb4e5fad9b617337245bd054d5d1affe00ef
June Skálletty <101154164+juneskalletty@users.noreply.github.com>
2024-11-14 18:10:50 -0300

    estrutura do projeto

---
+++ src/main/java/agenciaViagens/AgenciaViagensApplication.java
@@ -0,0 +1,11 @@
+package agenciaViagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaViagensApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgenciaViagensApplication.class, args);
+    }
+}
